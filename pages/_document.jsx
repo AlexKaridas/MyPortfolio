e81c562f070585240a02c199ea9ab36c1e16f052
AlexKaridas <karidasalex@gmail.com>
2022-06-22 18:01:50 +0300

    icon change

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,6 +6,10 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link
+            rel="shortcut icon"
+            href="https://cdn-icons.flaticon.com/png/512/3256/premium/3256085.png?token=exp=1655910920~hmac=fd6abdf66e2a4bfce71c4ccf02f3c91e"
+          />
+          <link
             href="https://pro.fontawesome.com/releases/v6.0.0-beta1/css/all.css"
             rel="stylesheet"
           />
